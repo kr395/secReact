f94b531f96478788f992d0164482a244bfe8dfdd
kr395 <75511425+kr395@users.noreply.github.com>
2024-02-12 12:53:40 +0000

    basic Todo List added

--- TodoList/TodoList.jsx
+++ TodoList/TodoList.jsx
@@ -2,5 +2,36 @@ import React from "react";
 import { useState } from "react";
 
 export default function TodoList() {
-  return <h1>Hello</h1>;
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}{" "}
+            <button
+              onClick={() => {
+                setTodos(todos.filter((_, i) => i !== index));
+              }}
+            >
+              ❌
+            </button>{" "}
+          </li>
+        ))}
+      </ul>
+      <input id="inputTodo" type="text" placeholder="Add Todo"></input>
+      <button
+        onClick={() => {
+          const inputTodo = document.getElementById("inputTodo").value;
+          document.getElementById("inputTodo").value = "";
+          setTodos((t) => [...t, inputTodo]);
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
 }
