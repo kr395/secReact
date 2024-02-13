403577b03cbe95296654b4a67b4432f68278a9dd
kr395 <75511425+kr395@users.noreply.github.com>
2024-02-13 12:30:51 +0000

    Styling Todo List

--- TodoList/TodoList.jsx
+++ TodoList/TodoList.jsx
@@ -2,36 +2,58 @@ import React from "react";
 import { useState } from "react";
 
 export default function TodoList() {
-  const [todos, setTodos] = useState([]);
-
+  const [todos, setTodos] = useState([
+    {
+      taskId: 1,
+      taskName: "Learn React",
+      status: [
+        "pending",
+        "inprogress",
+        "completed",
+        "cancelled",
+        "deleted",
+        "onhold",
+      ],
+      date: new Date().toLocaleDateString(),
+      startTime: new Date().getTime(),
+      endTime: null,
+      isStar: false,
+    },
+  ]);
   return (
-    <div>
-      <h1>Todo List</h1>
-
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}{" "}
-            <button
-              onClick={() => {
-                setTodos(todos.filter((_, i) => i !== index));
-              }}
-            >
-              ❌
-            </button>{" "}
-          </li>
-        ))}
-      </ul>
-      <input id="inputTodo" type="text" placeholder="Add Todo"></input>
-      <button
-        onClick={() => {
-          const inputTodo = document.getElementById("inputTodo").value;
-          document.getElementById("inputTodo").value = "";
-          setTodos((t) => [...t, inputTodo]);
-        }}
-      >
-        Add Todo
-      </button>
+    <div className="container">
+      <div className="hero">
+        <h4 id="introTxt">Let's See....</h4>
+        <h1 id="wtd">What TODO</h1>
+        <hr id="hr"></hr>
+      </div>
+      <div id="head">
+        <div id="addDiv">
+          <input id="inTaskName" type="text" placeholder="What to do?" />
+          <input id="inDate" type="date" placeholder="What to do?" />
+          <input id="inStartTime" type="time" placeholder="What to do?" />
+          <input id="inEndTime" type="time" placeholder="What to do?" />
+          <button id="reset">Reset</button>
+          <input id="addTask" type="button" value="Add Task" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="Task">
+          <h3>Task Details</h3>
+          <p>#</p>
+          <p>Task Name</p>
+          <p>Date</p>
+          <p>Start</p>
+          <p>End</p>
+          <p>Status</p>
+          <p>Edit</p>
+          <p>Remove</p>
+          <p>Star</p>
+        </div>
+      </div>
+      <div id="footer">
+        <p>End of the Page</p>
+      </div>
     </div>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import "/TodoList/TodoList.css";
 import UpdateObj from "/updateObjects/UpdateObj.jsx";
 import UpdateArray from "/updateObjects/UpdateArray.jsx";
 import TodoList from "/TodoList/TodoList.jsx";
 
 export default function App() {
   return (
-   // <UpdateObj />
+    // <UpdateObj />
     // <UpdateArray />
     <TodoList />
-         );
+  );
 }
